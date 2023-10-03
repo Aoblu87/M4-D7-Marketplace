451b9f73fa1b4384142e9661add614617f10551a
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-10-03 20:24:15 +0200

    creata sezione per modificare  e cancellare articoli

--- admin/admin.js
+++ admin/admin.js
@@ -3,7 +3,79 @@ let price = document.querySelector("#priceInput")
 let imageUrl = document.querySelector("#imageUrl")
 let description = document.querySelector("#descriptionInput")
 let brand = document.querySelector('#brandInput')
+let productsContainer = document.querySelector('#products-container')
 
+// fetch(`https://striveschool-api.herokuapp.com/api/product/`, {
+//     headers: {
+//         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFhZTkzYjk2OTNhMDAwMThiOWRlYjkiLCJpYXQiOjE2OTYyNjI0NTksImV4cCI6MTY5NzQ3MjA1OX0.zDkQ9wBotUv_FGzLQHZSRMthIEqAPb40vnYwaa5RBL4"
+//     }
+// })
+//     .then(r => r.json())
+//     .then(displayProducts)
+//     productsContainer = document.querySelector('#products-container')
+
+window.onload = async function () {
+    try {
+        allProducts = await getProducts()
+
+        console.log(allProducts)
+
+
+        // Varibili per identificare nodi
+        
+        let productsContainer = document.querySelector('#products-container')
+       
+
+
+       
+
+        // Mostro tutti i prodotti nel DOM
+        displayProducts(allProducts)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+// Funzione che richiama tutti i prodotti
+async function getProducts() {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFhZTkzYjk2OTNhMDAwMThiOWRlYjkiLCJpYXQiOjE2OTYyNjI0NTksImV4cCI6MTY5NzQ3MjA1OX0.zDkQ9wBotUv_FGzLQHZSRMthIEqAPb40vnYwaa5RBL4"
+            }
+        })
+        const jsonData = await response.json()
+        return jsonData
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// mostro nel DOM i risultati
+function displayProducts(data) {
+
+    productsContainer.innerHTML = data.map(({ _id, name, price, imageUrl, description }) => /*html*/`
+                    <div class="col-md-3">
+                        <div class="card h-100 border border-0">
+                        <a href="../product/product.html?id=${_id}">
+                        <img src="${imageUrl}" class="card-img-top" alt="${name}">
+                            <div class="card-body">
+                                <h5 class="card-title text-center"> <a href="../product/product.html?id=${_id}" class="link-underline link-underline-opacity-0 link-dark">
+                                ${name}</a></h5>
+                                <p class="card-text text-center">${price}€</p>
+                
+                            </div>
+                            <div class= "justify-content-evenly align-items-center">
+                                <button type="button" class="btn btn-transparent">Change</i></button>
+                                <button type="button" class="cartButton btn btn-transparent"  >Delete</i> </button>
+                            </div>
+                        </div>
+                    </div>
+                    `
+    ).join('')
+    
+}
 
 // Funzione che aggiunge prodotti 
 
@@ -35,9 +107,9 @@ async function addProduct(event) {
 
 
     if (response.ok) {
-        alert("Yohoo! Added Event")
-        // const data = await getAgenda()
-        // displayAgenda(data)
+        
+        const data = await getProducts()
+        displayProducts(data)
 
         for (const field of [title, price, imageUrl, description]) {
             field.value = ''
--- product/product.js
+++ product/product.js
@@ -1,56 +1,90 @@
-// const params = new URLSearchParams(window.location.search)
-// const id = params.get('id')
+const params = new URLSearchParams(window.location.search)
+const id = params.get('id')
+let detailsContainer = document.querySelector('#details-container')
+
+// window.onload = async function () {
+
+//     try {
+//         const product = await getDetailsProduct()
+
+//         detailsContainer = document.querySelector('#main-container')
+
+//         displayProduct(product)
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
 
-// fetch(`https://striveschool-api.herokuapp.com/books/${id}`)
-//     .then(r => r.json())
-//     .then(displayBook)
 
 
-// const bookCard = document.querySelector("#cardBook")
-
-// function displayBook(data) {
-//     console.log(data.title)
-//     console.log(data)
-//     bookCard.innerHTML += 
-//          /*html*/`
-//         <div class="col-md-3 d-flex g-5">
-//             <img src="${data.img}" class="img-fluid" alt="...">
-//         </div>
-//         <div class="col d-flex flex-column p-5 g-5">
-        
-//             <div class="col d-flex flex-column align-items-center">
-//                 <h5 class="f-6">${data.title}</h5>
-//                 <h6 class="">${data.price} €</h6>
-        
-//             </div>
-//             <div class="col d-flex align-items-center">
-//                 <button class="btn btn-outline-secondary dropdown-toggle" type="button"
-//                     data-bs-toggle="dropdown" aria-expanded="false">1</button>
-//                 <ul class="dropdown-menu dropdown-menu-end">
-//                     <select class="form-select" size="3" aria-label="Size 3 select example">
-//                         <option selected>1</option>
-//                         <option value="1">2</option>
-//                         <option value="2">3</option>
-//                         <option value="3">4</option>
-//                     </select>
-//                 </ul>
-//                 <button type="button" class="btn btn-success">Aggiungi al carrello</button>
-        
-        
-//             </div>
-        
-//         </div>
-                        
-                        
-                        
-//         `
 
 
-        
+// async function getDetailsProduct() {
+//     try {
+//         const response = await fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
 
-    
-
+//             headers: {
+//                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFhZTkzYjk2OTNhMDAwMThiOWRlYjkiLCJpYXQiOjE2OTYyNjI0NTksImV4cCI6MTY5NzQ3MjA1OX0.zDkQ9wBotUv_FGzLQHZSRMthIEqAPb40vnYwaa5RBL4"
+//             }
+//         })
+//         const jsonData = await response.json()
+//         return jsonData
+//     } catch (error) {
+//         console.log(error)
+//     }
 
 // }
+
+fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
+    headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFhZTkzYjk2OTNhMDAwMThiOWRlYjkiLCJpYXQiOjE2OTYyNjI0NTksImV4cCI6MTY5NzQ3MjA1OX0.zDkQ9wBotUv_FGzLQHZSRMthIEqAPb40vnYwaa5RBL4"
+    }
+})
+    .then(r => r.json())
+    .then(displayProduct)
+
+function displayProduct(data) {
+
+    detailsContainer.innerHTML += /*html*/`
+        <div class="col-md-3 d-flex g-5">
+            <img src="${data.imageUrl}" class="img-fluid" alt="${data.name}">
+        </div>
+        <div class="col d-flex flex-column p-5 g-5">
+        
+            <div class="col d-flex flex-column align-items-center">
+                <h5 class="f-6">${data.name}</h5>
+                <h6 class="">${data.price} €</h6>
+        
+            </div>
+            <div class="col d-flex align-items-center">
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">1</button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <select class="form-select" size="3" aria-label="Size 3 select example">
+                        <option selected>1</option>
+                        <option value="1">2</option>
+                        <option value="2">3</option>
+                        <option value="3">4</option>
+                    </select>
+                </ul>
+                <button type="button" class="btn btn-success">Aggiungi al carrello</button>
+        
+        
+            </div>
+        
+        </div>
+        
+`
+
+}
+
+
+
+
+
+
+
+
 
