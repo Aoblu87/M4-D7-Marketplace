44a92d5f554d02acdc273a47fda911aa509b8e08
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-10-05 14:53:56 +0200

    creato spinner nella pagina prodotto

--- product/product.js
+++ product/product.js
@@ -1,7 +1,16 @@
 const params = new URLSearchParams(window.location.search)
 const id = params.get('id')
-let detailsContainer = document.querySelector('#details-container')
+let detailsContainer = document.querySelector('#product-result > div')
 
+
+detailsContainer.innerHTML = /*html*/ `
+        <div id="loader" class="loading p-5 mt-5">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+       
+    `
 
 
 fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
@@ -11,38 +20,51 @@ fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`, {
 })
     .then(r => r.json())
     .then(displayProduct)
+    .finally(() => {
+        detailsContainer.querySelector("#loader").remove();
+      });
 
 function displayProduct(data) {
 
-    detailsContainer.innerHTML += /*html*/`
-        
+    detailsContainer.innerHTML = /*html*/`
+    
+                <div  class="card mb-3 p-3">
+                    <div class="row justify-content-between mt-2">
+                        <div class="col-3 d-flex align-items-center p-3">
+                            <a class="link-body-emphasis" href="../index.html">
+                                    <i class="fs-3 bi bi-arrow-left"></i>
+                            </a>
 
-                <div class="card mb-3">
-                    <div class="row">
-                        <div class="col">
+                            <p class="em ms-3 m-0">
+                            <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover text-dark link-body-emphasis "
+                                    href="../index.html">Back to all Plants</a>
 
-                            <i class="bi bi-arrow-left"></i>
+                            </p>
                         </div>
-                        <div class="col">
-                            <h5>Back to all Plants</h5>
-                        </div>
-                        <div class="col">
+                        <div id="favorites-icon" class="col-1 d-flex position-relative fs-3 p-3">
 
-                            <i class="bi bi-suit-heart"></i>
+                                <i id="favorite-unsaved" class="bi bi-suit-heart"></i>
+                                <i id="favorite-saved" class="bi bi-suit-heart-fill d-none"></i>
                         </div>
                     </div>
                     <div class="row g-0">
                         <div class="col-md-4">
-                            <img src="${data.imageUrl}" alt="${data.title}">
+                            <img class="img-fluid" src="${data.imageUrl}" alt="${data.title}">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">${data.name}</h5>
-                                <h1>${data.price}€</h1>
-                                <p class="card-text">${data.description}</p>
-                                <h4 class="card-title">${data.brand}</h4>
-                                <button>Add to Cart</button>
-                                <button>Wishlist</button>
+                                    <h3 class="card-title p-3">${data.name}</h3>
+                                    <p class="card-title text-body-secondary ps-3">${data.brand}</p>
+                                    <hr>
+                                    <div class="col d-flex justify-content-end ps-3 my-4">
+                                        <p class="h3 m-0">${data.price} €</p>
+
+                                    </div>
+                                    <p class="card-text p-3"></p>
+                                <div class="col d-flex justify-content-end p-3">
+                                    <button type="button" class="btn btn-dark">Add to Cart</button>
+
+                                </div>
                             </div>
                         </div>
                     </div>
