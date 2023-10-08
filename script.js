4ce118261ef38e1935b108f4839a0427dbfef37d
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-10-08 14:27:42 +0200

    lavoro funzione per aggiungere preferitti

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let contentContainer = document.querySelector('#main-content >div')
 // TASTO CANCELLA ARTICOLO
 let cancelItem = document.querySelector('#cancel-item')
 
-
+// TASTO AGGIUNGI FAVORITI
+let wishlistBtn = document.querySelectorAll('.wishlist-btn')
 
 let allProducts = []
-
+let favorites =[]
 
 
 window.onload = async function () {
@@ -36,9 +37,10 @@ window.onload = async function () {
 
 
         // Varibili per identificare nodi
-      
+
         contentContainer = document.querySelector('#main-content> div')
-    
+
+
 
         // Mostro tutti i prodotti nel DOM
         displayResult(allProducts)
@@ -66,6 +68,7 @@ async function getProducts() {
     } catch (error) {
         console.log(error)
     }
+
 }
 
 
@@ -75,7 +78,7 @@ async function getProducts() {
 function displayResult(data) {
 
     contentContainer.innerHTML = data.map(({ _id, name, price, imageUrl, description }) => /*html*/`
-                                <div id="card" class="row p-2 bg-white border rounded my-3">
+                                <div id="_${_id}" class="item row p-2 bg-white border rounded my-3">
                                     <div class="col-md-3 mt-1">
                                         <a href="../product/product.html?id=${_id}">
                                         <img  class="img-product img-fluid img-responsive rounded product-image"src="${imageUrl}"></a>
@@ -91,17 +94,42 @@ function displayResult(data) {
                                         </div>
                                         <div class="d-flex flex-column mt-4">
                                             <a class="btn btn-primary btn-sm" href="../product/product.html?id=${_id}" role="button">Details</a>
-                                            <button class="btn btn-outline-primary btn-sm mt-2" type="button">Add to wishlist</button>
+                                            <button  class="wishlist-btn btn btn-outline-primary btn-sm mt-2" type="button" onclick="addFavorites('${_id}')">Add to wishlist</button>
                                         </div>
                                     </div>
                                 </div>
                                 `
     ).join('')
 }
-                                        
 
-                                                
-        
+
+// funzione per creare array degli articoli aggiunti alla wishlist
+
+
+function addFavorites(id){
+    const itemSelected = allProducts.find(item => item._id === id)
+console.log(itemSelected)
+
+
+    favorites.push({
+        ...itemSelected,
+        quantity: 1
+    })
+
+//     if (favorites.quantity === 1) {
+            
+//     favorites.quantity++
+
+// } else { // creo array di tutti i libri selezionati
+//     favorites.push({
+//         ...itemSelected,
+//         quantity: 1
+//     })
+// }
+
+    console.log(favorites)
+}
+
 
 
 
